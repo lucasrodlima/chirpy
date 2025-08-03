18b70a93718c6c207c271fcf11009a27d65085f2
lucasrodlima <lucasrodlima27@gmail.com>
2025-08-03 20:01:10 -0300

    validate_chirp cleans message

--- validate_chirp.go
+++ validate_chirp.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"net/http"
+	"strings"
 )
 
 func handlerValidateChirp(w http.ResponseWriter, r *http.Request) {
@@ -10,12 +11,11 @@ func handlerValidateChirp(w http.ResponseWriter, r *http.Request) {
 		Body string `json:"body"`
 	}
 	type returnVals struct {
-		Valid bool `json:"valid"`
+		CleanedBody string `json:"cleaned_body"`
 	}
 
 	defer r.Body.Close()
 	decoder := json.NewDecoder(r.Body)
-
 	params := parameters{}
 	err := decoder.Decode(&params)
 	if err != nil {
@@ -23,12 +23,31 @@ func handlerValidateChirp(w http.ResponseWriter, r *http.Request) {
 			"couldn't decode parameters", err)
 		return
 	}
+
 	const maxChirpLength = 140
 	if len(params.Body) > maxChirpLength {
 		respondWithError(w, http.StatusBadRequest, "Chirp is too long", err)
 		return
 	}
+
+	cleanChirp := cleanMessage(params.Body)
+
 	respondWithJson(w, http.StatusOK, returnVals{
-		Valid: true,
+		CleanedBody: cleanChirp,
 	})
+}
+
+func cleanMessage(msg string) string {
+	msgWords := strings.Split(msg, " ")
+	badWords := []string{"kerfuffle", "sharbert", "fornax"}
+
+	for i, word := range msgWords {
+		for _, badWord := range badWords {
+			if strings.ToLower(word) == badWord {
+				msgWords[i] = "****"
+			}
+		}
+	}
+	cleanStr := strings.Join(msgWords, " ")
+	return cleanStr
 }
